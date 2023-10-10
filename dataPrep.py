5b024f585a3a94f804bd702fbfd8ab347b654f68
Elispe <elisabet.perotti@gmail.com>
2023-10-10 16:10:47 -0600

    Prepare data

---
+++ dataPrep.py
@@ -0,0 +1,81 @@
+"""
+Extracts ride request data from the Taxi and Limousine Commission, Manhattan, NY
+Returns stationary data (demand matrix) for a specific time window
+"""
+import pandas as pd
+import numpy as np
+import datetime
+
+trips_df = pd.read_parquet('yellow_tripdata_2022-03.parquet', engine='pyarrow')
+# Remove not needed features
+trips_df["trip_duration"] = trips_df["tpep_dropoff_datetime"] - trips_df["tpep_pickup_datetime"]
+trips_df = trips_df.drop(
+    ["VendorID", "passenger_count", "tpep_dropoff_datetime", "RatecodeID", "store_and_fwd_flag", "payment_type",
+     "extra",
+     "mta_tax", "tip_amount", "tolls_amount", "total_amount", "improvement_surcharge", "congestion_surcharge",
+     "airport_fee"], axis=1)
+
+def create_R(dd_in, dd_fin, h_in, time_period, numNodes):
+    # Choose which days you want the data for
+    yyyy = "2022"
+    mm = "03"
+    mask = (trips_df['tpep_pickup_datetime'] >= "-".join([yyyy, mm, dd_in])) & \
+           (trips_df['tpep_pickup_datetime'] < "-".join([yyyy, mm, dd_fin]))
+    selected_trips = trips_df.loc[mask]
+    ordered_trips = selected_trips.sort_values(by="tpep_pickup_datetime")
+    # Read in locationID data
+    map_df = pd.read_csv('taxiZones.csv')
+
+    # Remove not needed features
+    map_df = map_df.drop("service_zone", axis=1)
+    manhattan_loc_df = map_df.loc[map_df['Borough'] == 'Manhattan']
+    manhattan_loc_df = manhattan_loc_df.drop("Borough", axis=1)
+
+    # Group zones into 18 areas
+    darea = {
+        'LocationID': [12, 13, 261, 87, 88, 209, 125, 211, 144, 231, 45, 148, 232, 158, 249, 113, 114, 79, 4, 68, 90,
+                       234, 107, 224, 246, 100, 186, 164, 170, 137, 50, 48, 230, 163, 161, 162, 229, 233, 143, 142,
+                       239, 141, 140, 237, 24, 151, 238, 236, 263, 262, 166, 41, 74, 75, 42, 152, 116],
+        'Area': [1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 6, 6, 6, 7, 7, 8, 8, 8, 9, 9, 9, 10, 10, 10,
+                 10, 11, 11, 11, 11, 12, 12, 12, 13, 13, 13, 14, 14, 14, 15, 15, 15, 16, 16, 17, 17, 18, 18, 18]}
+    area_df = pd.DataFrame(data=darea)
+    area_df = pd.merge(manhattan_loc_df, area_df, how='left', left_on=['LocationID'], right_on=['LocationID'])
+    area_df = area_df.loc[:, ['LocationID', 'Area']]
+    final_df = pd.merge(ordered_trips, area_df, how='left', left_on=['PULocationID'], right_on=['LocationID'])
+    final_df = final_df.drop('LocationID', axis=1)
+    final_df = pd.merge(final_df, area_df, how='left', left_on=['DOLocationID'], right_on=['LocationID'])
+    final_df = final_df.drop('LocationID', axis=1)
+    final_df = final_df.rename(columns={"Area_x": "PUArea", "Area_y": "DOArea"})
+    final_df = final_df.drop(["PULocationID", "DOLocationID"], axis=1)
+    maskNaN = (final_df['PUArea'] > 0) & (final_df['DOArea'] > 0)
+    final_df = final_df.loc[maskNaN]
+    final_df['PUArea'] = final_df['PUArea'].astype('Int64')
+    final_df['DOArea'] = final_df['DOArea'].astype('Int64')
+
+    # Keep only trips within a selected number of areas in Manhattan
+    mask_areas = (final_df['PUArea'] <= numNodes) & (final_df['DOArea'] <= numNodes) & (final_df['PUArea'] != final_df['DOArea'])
+    final_areas_df = final_df.loc[mask_areas]
+
+    # Keep only ride requests received within the time window specified below. Choose the time period.
+    ini = datetime.datetime(int(yyyy), int(mm), int(dd_in), h_in, 0, 0)
+    delta_min = datetime.timedelta(seconds=time_period * 60)
+
+    mask_areas_min = (final_areas_df['tpep_pickup_datetime'] >= ini) & (
+            final_areas_df['tpep_pickup_datetime'] < ini + delta_min)
+    final_areas_min = final_areas_df.loc[mask_areas_min]
+    numRequests = len(final_areas_min)
+    PU_arr = final_areas_min['PUArea'].tolist()
+    DO_arr = final_areas_min['DOArea'].tolist()
+
+    print(str(numRequests) + " requests between " + str(ini) + " and " + str(ini + delta_min))
+
+    # Build original demand matrix
+    theta = np.array([np.zeros(numNodes) for _ in range(numNodes)])
+    for i in range(len(PU_arr)):
+        if not PU_arr[i] == DO_arr[i]:
+            theta[PU_arr[i] - 1][DO_arr[i] - 1] += 1
+
+    return theta
+
+
+
