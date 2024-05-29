3a0144628c2dd5a50f254a3c5d4372cb8c89c74d
Elispe <elisabet.perotti@gmail.com>
2024-05-29 16:42:12 -0600

    Online implementation. Dispatch at every iteration.

--- main.py
+++ main.py
@@ -4,35 +4,37 @@ import numpy as np
 import cvxpy as cp
 from generateDF import numNodes, tau, final_df
 import time
-import random
+import matplotlib.pyplot as plt
 
-rng = np.random.default_rng()
-
-# starting price TO DO: educated guess?
-Y0 = np.array([np.zeros(numNodes) for n in range(numNodes)])
-for i in range(numNodes):
-    for j in range(numNodes):
-        Y0[i][j] = random.uniform(0, 1)
+rng = np.random.default_rng(seed=1)
 
 # Introduce parameters
-beta = 0.04  # operational cost ($/min)
+beta = 0.1  # operational cost ($/min)
 ei = np.ones(numNodes)
-gamma = 0.1
-eta = 0.1 # step size
-alpha = 0.1 # elasticity
-L0 = np.diag([0 for i in range(numNodes)])
-X0 = np.array([[random.randint(0,2) for n in range(numNodes)]for n in range(numNodes)])
-print("X0", X0)
+alpha = 0.5  # elasticity
+gamma1 = 0.1 # regularization param
+gamma2 = 0.01
+eta = 0.02  # stepsize
 
-max_riding_time=0
+#Initialize L0, X0 and Y0
+L0 = np.diag([0 for i in range(numNodes)])
+print("L0\n", L0)
+theta = np.array([[1.2, 0.8, 0.4], [0.3, 2.2, 0.4], [2.5, 0.5, 2.0]]);
+#X0 = np.array([[rng.integers(low=10, high=30) for n in range(numNodes)]for n in range(numNodes)])
+X0 = theta
+print("X0\n", X0)
+Y0 = np.diag([0 for i in range(numNodes)])
+print("Y0\n", Y0)
+
+max_riding_time = 0
 for h in range(len(tau)):
     max_riding_time = max(np.max(tau[h]), max_riding_time)
 max_riding_time = max_riding_time//60
 
-# Initialize time counter
+#Initialize time counter
 counters = {}
 for i in range(max_riding_time):
-    counters[i] = np.array([random.randint(0,5) for i in range(numNodes)])
+    counters[i] = rng.integers(low=20, high=100, size=numNodes)
 
 for i in range(numNodes):
     for j in range(numNodes):
@@ -47,56 +49,55 @@ print("fleet size:", n_veh)
 
 data = Data(final_df, tau, numNodes)
 
-# deploy price policy Y0, observe R0
+# deploy price policy Y0 at every iteration, observe R0
 def SGD(h, min, X0, Y0, L0):
+    # riding time
+    print("tau\n", tau[h] // 60)
+    # nominal price
+    P = tau[h] // 60 * beta * 20
+    print("nominal price\n", P)
     # idling vehicles
     nu = counters[0]
     print("idling", nu)
-    theta = data.draw_R(h, min)
-    print("theta", theta)
+    theta = data.avg_R(h, min, period_min=1)
+    print("theta\n", theta)
+
+    print("**SGD results**")
+
+    # Online algo
     R0 = theta - alpha * Y0
     for i in range(numNodes):
         for j in range(numNodes):
             if R0[i][j]<=0:
-                R0[i][j]=0
-    print("R0", R0)
-    print("tau", tau[h]//60)
+                R0[i][j] = 0
+                print("Warning: negative effective demand")
+    print(" effective demand\n", R0)
 
-    gammaX0 = [[xx*gamma for xx in r] for r in X0]
-    gradX = beta * tau[h]//60 + L0 + gammaX0
-    gradY = -R0 + gamma * Y0
-    gradL = X0 - R0 - gamma * L0
+    z = theta-alpha*Y0
+    gradX = beta * tau[h] // 60 - L0 + gamma1 * X0
+    gradY = -z + gamma1 * Y0
+
+    L0 = L0+10*eta*(z-X0-gamma2*L0)
+    L0[L0 < 0] = 0
 
     Y = cp.Variable((numNodes, numNodes), "Y")
     X = cp.Variable((numNodes, numNodes), "X")
-    L = cp.Variable((numNodes, numNodes), "L")  # dual variable
 
-    objX = cp.sum_squares(X - X0 + eta * gradX)
-    constraintsX = []
-    constraintsX += [X @ ei == nu]
-    constraintsX += [X >= 0]
-    problemX = cp.Problem(cp.Minimize(objX), constraintsX)
-    problemX.solve()
-
-    objY = cp.sum_squares(Y - Y0 + eta * gradY)
-    constraintsY = []
-    constraintsY += [Y >= 0]
-    constraintsY += [Y <= 1]
-    problemY = cp.Problem(cp.Minimize(objY), constraintsY)
-    problemY.solve()
-
-    objL = cp.sum_squares(L - L0 + eta * gradL)
-    constraintsL = []
-    constraintsL += [L >= 0]
-    constraintsL += [L <= 1]
-    problemL = cp.Problem(cp.Minimize(objL), constraintsL)
-    problemL.solve()
+    obj = cp.sum_squares(X - X0 + eta * gradX) + cp.sum_squares(Y - Y0 + eta * gradY)
+    constraints = []
+    constraints += [X @ ei == nu]
+    constraints += [X >= 0]
+    constraints += [Y >= -10]
+    constraints += [Y <= 10]
+    problem = cp.Problem(cp.Minimize(obj), constraints)
+    problem.solve()
 
     X0 = X.value
     Y0 = Y.value
-    L0 = L.value
-    print("X0", X0)
-    print("Y0", Y0)
+
+    print("X\n", X0)
+    print("Y\n", Y0)
+    print("L\n", L0)
 
     # Generate integer actions
     X0_int = []
@@ -109,12 +110,14 @@ def SGD(h, min, X0, Y0, L0):
             X0_int.append(np.zeros(numNodes, dtype=int))
         else:
             countEl = np.zeros(numNodes, dtype=int)
-            p = row / size
-            print("prob", p)
-            res = rng.choice(a, int(np.round(size)), True, np.abs(p))
+            p = row / np.linalg.norm(row)
+            print("prob", p*p)
+            print("sum prob", np.linalg.norm(p))
+            res = rng.choice(a, int(np.round(size)), True, p*p)
             for el in res:
                 countEl[el] += 1
             X0_int.append(countEl)
+    print("EV dispatch\n", X0_int)
 
     # Update counters
     for t in range(max_riding_time - 1):
@@ -129,15 +132,15 @@ def SGD(h, min, X0, Y0, L0):
             travel_time = tau[h][i][j]//60
             counters[travel_time - 1][j] += X0_int[i][j]
 
-    print("X0_int", X0_int)
-    return X0_int, Y0, L0
+    return X0, Y0, L0
 
 start_time = time.time()
 
 sim_window = 5*60 #minutes
-h_in = 10
-min_in = 1
+h_in = 18
+min_in = 0
 for minu in range(sim_window):
+    print("***Time", h_in, ":", min_in)
     X0, Y0, L0 = SGD(h_in, min_in, X0, Y0, L0)
     if min_in != 59:
         min_in += 1
@@ -145,15 +148,14 @@ for minu in range(sim_window):
         h_in += 1
         min_in = 0
 
-    # Fleet size test
-    n_veh = 0
+    #Fleet size test
+    n_veh_count = 0
     for i in range(len(counters)):
-        n_veh += np.sum(counters[i])
-    print("fleet size:", n_veh)
+        n_veh_count += np.sum(counters[i])
+    if n_veh_count != n_veh:
+        raise Exception("number vehicles not conserved")
 
 print("--- %s seconds ---" % (time.time() - start_time))
-
-
 
 
 
